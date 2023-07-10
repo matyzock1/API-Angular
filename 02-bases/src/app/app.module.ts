062a6f4c96eb9b84dc1b813887a14be746c3d77e
matyzock1 <matyzock@gmail.com>
2023-07-09 20:30:54 -0400

    Detalles

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
 
-
-
 @NgModule({
   declarations: [
     AppComponent,
